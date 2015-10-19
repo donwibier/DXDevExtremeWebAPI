16a87a03dd01de1774c1a340f7cc60305a19f245
donwibier <don@anykey.nl>
2015-10-19 21:07:16 +0200

    publish fixes

--- DXDevExtremeWebAPI/DXDevExtremeClient.Shared/views/values.js
+++ DXDevExtremeWebAPI/DXDevExtremeClient.Shared/views/values.js
@@ -1,5 +1,27 @@
 ﻿DXDevExtremeClient.Values = function (params) {
-
+    /*
+        var _valuesSource = new DevExpress.data.CustomStore({
+    	pageSize: 20,
+    	load: function (options) {
+    	    return DXDevExtremeClient.db.get('Values', '', null, null);
+       },
+    	byKey: function(key) {
+    	    return DXDevExtremeClient.db.get('Values', encodeURIComponent(key), null, null, datasourceError);
+    	},
+    	insert: function (values) {
+    	    return DXDevExtremeClient.db.post('Values', '', values, null, datasourceError);
+        },
+    	
+    	update: function (key, values) {
+    	    return DXDevExtremeClient.db.put('Values', encodeURIComponent(key), values, null, datasourceError);
+        },
+    	
+    	remove: function (key) {
+    	    return DXDevExtremeClient.db.del('Values', encodeURIComponent(key), null, null, datasourceError);
+        }
+    	
+    });
+    */
     var _datasource = ko.observableArray();
 
     function getValues() {
