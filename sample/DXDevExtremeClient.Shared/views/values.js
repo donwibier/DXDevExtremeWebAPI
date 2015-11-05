b0048e1dd5b09927ecd92672cc813b1920ef4308
Don Wibier <don@anykey.nl>
2015-11-05 21:00:11 +0100

    Datasource with custom store test

--- sample/DXDevExtremeClient.Shared/views/values.js
+++ sample/DXDevExtremeClient.Shared/views/values.js
@@ -1,35 +1,77 @@
 ﻿DXDevExtremeClient.Values = function (params) {
-    
+    "use strict";
     //var _datasource = new DevExpress.data.DataSource({
-    //	pageSize: 20,
-    //	load: function (options) {
-    //	    return DXDevExtremeClient.db.get('Values', '', null, null, datasourceError);
-    //   },
-    //	byKey: function(key) {
-    //	    return DXDevExtremeClient.db.get('Values', encodeURIComponent(key), null, null, datasourceError);
-    //	},
-    //	insert: function (values) {
-    //	    return DXDevExtremeClient.db.post('Values', '', values, null, datasourceError);
-    //    },
-    	
-    //	update: function (key, values) {
-    //	    return DXDevExtremeClient.db.put('Values', encodeURIComponent(key), values, null, datasourceError);
-    //    },
-    	
-    //	remove: function (key) {
-    //	    return DXDevExtremeClient.db.del('Values', encodeURIComponent(key), null, null, datasourceError);
-    //    }
-    	
+    //    store: new DevExpress.data.CustomStore({
+    //            load: function (options) {
+    //                var d = $.Deferred();                    
+    //                DXDevExtremeClient.db.get('Values', '', null,
+    //                    function (data) {
+    //                        d.resolve(data);
+    //                    },
+    //                    function (err) {
+    //                        d.reject(err);
+    //                    });
+    //                return d.promise();
+    //            },
+    //            byKey: function (key) {
+    //                var d = $.Deferred();
+    //                DXDevExtremeClient.db.get('Values', encodeURIComponent(key), null,
+    //                    function (data) {
+    //                        d.resolve(data);
+    //                    },
+    //                    function (err) {
+    //                        datasourceError(err);
+    //                        d.reject(err);
+    //                    });
+    //                return d.promise();
+    //            }/*,
+    //            insert: function (values) {
+    //                var d = $.Deferred();
+    //                DXDevExtremeClient.db.post('Values', '', values,
+    //                    function (data) {
+    //                        d.resolve(data);
+    //                    },
+    //                    function (err) {
+    //                        datasourceError(err);
+    //                        d.reject(err);
+    //                    });
+    //                return d.promise();
+    //            },
+    //            update: function (key, values) {
+    //                var d = $.Deferred();
+    //                DXDevExtremeClient.db.put('Values', encodeURIComponent(key), values,
+    //                    function (data) {
+    //                        d.resolve(data);
+    //                    },
+    //                    function (err) {
+    //                        datasourceError(err);
+    //                        d.reject(err);
+    //                    });
+    //                return d.promise();
+    //            },
+    //            remove: function (key) {
+    //                var d = $.Deferred();
+    //                DXDevExtremeClient.db.del('Values', encodeURIComponent(key), null,
+    //                    function (data) {
+    //                        d.resolve(data);
+    //                    },
+    //                    function (err) {
+    //                        datasourceError(err);
+    //                        d.reject(err);
+    //                    });
+    //                return d.promise();
+    //            }*/
+    //        })    	
     //});
 
-    function datasourceError(err) {
-        var msg = (err.responseJSON && err.responseJSON.Message) ? err.responseJSON.Message : err;
-        DevExpress.ui.notify('The server returned an error:' + msg, 'error', 3000);
-    }
+    //function datasourceError(err) {
+    //    var msg = (err.responseJSON && err.responseJSON.Message) ? err.responseJSON.Message : err;
+    //    DevExpress.ui.notify('The server returned an error:' + msg, 'error', 3000);
+    //}
+
     var _datasource = ko.observableArray();
 
-    function getValues() {
-        //_datasource.load();
+    function getValues() {        
         db.get('Values', '', null, function (data) {           
             _datasource(data);
         },
