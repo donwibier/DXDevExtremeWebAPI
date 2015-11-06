2236b894358d591317c0a4f00f39a1e5341f8c3e
Don Wibier <don@anykey.nl>
2015-11-06 12:34:58 +0100

    Datasource approach

--- sample/DXDevExtremeClient.Shared/views/values.js
+++ sample/DXDevExtremeClient.Shared/views/values.js
@@ -1,83 +1,78 @@
 ﻿DXDevExtremeClient.Values = function (params) {
     "use strict";
-    //var _datasource = new DevExpress.data.DataSource({
-    //    store: new DevExpress.data.CustomStore({
-    //            load: function (options) {
-    //                var d = $.Deferred();                    
-    //                DXDevExtremeClient.db.get('Values', '', null,
-    //                    function (data) {
-    //                        d.resolve(data);
-    //                    },
-    //                    function (err) {
-    //                        d.reject(err);
-    //                    });
-    //                return d.promise();
-    //            },
-    //            byKey: function (key) {
-    //                var d = $.Deferred();
-    //                DXDevExtremeClient.db.get('Values', encodeURIComponent(key), null,
-    //                    function (data) {
-    //                        d.resolve(data);
-    //                    },
-    //                    function (err) {
-    //                        datasourceError(err);
-    //                        d.reject(err);
-    //                    });
-    //                return d.promise();
-    //            }/*,
-    //            insert: function (values) {
-    //                var d = $.Deferred();
-    //                DXDevExtremeClient.db.post('Values', '', values,
-    //                    function (data) {
-    //                        d.resolve(data);
-    //                    },
-    //                    function (err) {
-    //                        datasourceError(err);
-    //                        d.reject(err);
-    //                    });
-    //                return d.promise();
-    //            },
-    //            update: function (key, values) {
-    //                var d = $.Deferred();
-    //                DXDevExtremeClient.db.put('Values', encodeURIComponent(key), values,
-    //                    function (data) {
-    //                        d.resolve(data);
-    //                    },
-    //                    function (err) {
-    //                        datasourceError(err);
-    //                        d.reject(err);
-    //                    });
-    //                return d.promise();
-    //            },
-    //            remove: function (key) {
-    //                var d = $.Deferred();
-    //                DXDevExtremeClient.db.del('Values', encodeURIComponent(key), null,
-    //                    function (data) {
-    //                        d.resolve(data);
-    //                    },
-    //                    function (err) {
-    //                        datasourceError(err);
-    //                        d.reject(err);
-    //                    });
-    //                return d.promise();
-    //            }*/
-    //        })    	
-    //});
+     
+    var _datasource = new DevExpress.data.DataSource({
+        pageSize: 20,
+        store: new DevExpress.data.CustomStore({
+            load: function (options) {
+                var d = $.Deferred();
+                DXDevExtremeClient.db.get('Values', '', null,
+                    function (data) {
+                        d.resolve(data);
+                    },
+                    function (err) {
+                        d.reject(err);
+                    });
+                return d.promise();
+            },
+            byKey: function (key) {
+                var d = $.Deferred();
+                DXDevExtremeClient.db.get('Values', encodeURIComponent(key), null,
+                    function (data) {
+                        d.resolve(data);
+                    },
+                    function (err) {
+                        datasourceError(err);
+                        d.reject(err);
+                    });
+                return d.promise();
+            }/*,
+                insert: function (values) {
+                    var d = $.Deferred();
+                    DXDevExtremeClient.db.post('Values', '', values,
+                        function (data) {
+                            d.resolve(data);
+                        },
+                        function (err) {
+                            datasourceError(err);
+                            d.reject(err);
+                        });
+                    return d.promise();
+                },
+                update: function (key, values) {
+                    var d = $.Deferred();
+                    DXDevExtremeClient.db.put('Values', encodeURIComponent(key), values,
+                        function (data) {
+                            d.resolve(data);
+                        },
+                        function (err) {
+                            datasourceError(err);
+                            d.reject(err);
+                        });
+                    return d.promise();
+                },
+                remove: function (key) {
+                    var d = $.Deferred();
+                    DXDevExtremeClient.db.del('Values', encodeURIComponent(key), null,
+                        function (data) {
+                            d.resolve(data);
+                        },
+                        function (err) {
+                            datasourceError(err);
+                            d.reject(err);
+                        });
+                    return d.promise();
+                }*/
+        })
+    });
 
-    //function datasourceError(err) {
-    //    var msg = (err.responseJSON && err.responseJSON.Message) ? err.responseJSON.Message : err;
-    //    DevExpress.ui.notify('The server returned an error:' + msg, 'error', 3000);
-    //}
+    function datasourceError(err) {
+        var msg = (err.responseJSON && err.responseJSON.Message) ? err.responseJSON.Message : err;
+        DevExpress.ui.notify('The server returned an error:' + msg, 'error', 3000);
+    }
 
-    var _datasource = ko.observableArray();
-
-    function getValues() {        
-        db.get('Values', '', null, function (data) {           
-            _datasource(data);
-        },
-        function (err) {
-            DevExpress.ui.notify('The server returned an error' + err, 'error', 3000);
-        });
+    function getValues() {
+        _datasource.load();
     }
 
     var viewModel = {
