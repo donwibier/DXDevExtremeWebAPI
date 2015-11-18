0fe6d1feac47e67302b6a123cd3ed6a9ba9d99ab
Don Wibier <don@anykey.nl>
2015-11-18 15:53:51 +0100

    rearrnged some code in films.js to copy and paste faster

--- sample/DXDevExtremeClient.Shared/views/films.js
+++ sample/DXDevExtremeClient.Shared/views/films.js
@@ -1,15 +1,13 @@
 ﻿DXDevExtremeClient.films = function (params) {
-    /*
+    
     var ds = ko.observableArray();
 
     function datasourceError(err) {
         var msg = (err.responseJSON && err.responseJSON.Message) ? err.responseJSON.Message : err;
         DevExpress.ui.notify('The server returned an error:' + msg, 'error', 3000);
     }
-    */
+
     function viewShownEvent() {
-        db.Films.load();
-        /*
         db.get('Films', '', null,
                     function (data) {
                         ds(data);
@@ -18,13 +16,21 @@
                         if (!sender.authorizeError(err))
                             datasourceError(err, sender);
                     });
-        */
     }
 
     var viewModel = {
-        dataSource: /*ds*/db.Films,        
+        dataSource: ds,
         viewShown: viewShownEvent
     };
+    /*
+    function viewShownEvent() {
+        db.Films.load();
+    }
 
+    var viewModel = {
+        dataSource: db.Films,        
+        viewShown: viewShownEvent
+    };
+    */
     return viewModel;
 };
